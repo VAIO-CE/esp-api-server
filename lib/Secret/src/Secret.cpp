e3f4c94250291447d4c3c5c058cdd0029a0b12c4
Satya Ananda <dev@satyaand.com>
2024-11-26 19:35:30 +0700

    fix(CICD): publish secrets for actions build

---
+++ lib/Secret/src/Secret.cpp
@@ -0,0 +1,4 @@
+#include "Secret.h"
+
+const char *Secret::AP_SSID = "ESPGen";
+const char *Secret::AP_PASS = "GIGACHAD";
---
+++ lib/Secret/src/Secret.h
@@ -0,0 +1,11 @@
+#ifndef SECRET_H
+#define SECRET_H
+
+class Secret
+{
+public:
+  static const char *AP_SSID;
+  static const char *AP_PASS;
+};
+
+#endif
